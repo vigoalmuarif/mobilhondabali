256beb09bc44050f116eb3789f37796871a70000
vigoalmuarif <vigoalmuarif@gmail.com>
2024-02-22 15:02:28 +0700

    .

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,7 +1,7 @@
 export default function robots() {
     return {
       rules: {
-        userAgent: ['Googlebot','Applebot', 'Bingbot'],
+        userAgent: '*',
         allow: '/',
         disallow: '/admin/',
       },
